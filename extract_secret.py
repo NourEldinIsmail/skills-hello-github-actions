6a763ce1d43c701b98f622f7de5dcbc8d3593f53
Nour El-Din Ahmed Hussein <nour.sehs.3@gmail.com>
2025-09-24 16:58:07 +0300

    Update extract_secret.py

--- extract_secret.py
+++ extract_secret.py
@@ -3,6 +3,9 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         input_string = sys.argv[1]
+        # add $ after the first character
+        manipulated_string = input_string[0] + '$' + input_string[1:]
         print(f"Received string: {input_string}")
+        print(f"Manipulated string: {manipulated_string}")
     else:
         print("No string argument provided")
